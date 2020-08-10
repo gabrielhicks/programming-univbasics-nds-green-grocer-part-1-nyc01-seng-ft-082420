3ba44f55fdd8d682d46a1d07dbb3d20f2b4b625e
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-10 13:06:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
-    # if item[name] == name
+    # if item[:item] == name
       binding.pry
     # end
   end
