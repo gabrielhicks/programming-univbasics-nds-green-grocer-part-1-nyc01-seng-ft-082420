0f9172327651c16fea6928694ecbc08d9b784dd6
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-10 13:36:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,15 +14,19 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  new_cart = []
-  names = cart.each do |item|
+  
+  items = cart.each do |item|
     name = item[:item]
     listed_item = find_item_by_name_in_collection(name, cart)
     
     if listed_item[:count] == nil
+      
       listed_item[:count] = 1
+      
     else
+      
     listed_item[:count] += 1
+    
     end
     
   end
